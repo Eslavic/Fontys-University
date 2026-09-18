d45a6146fa28bf6bf12ceeeb2776261630dc19fe
Eslavic <deslavajose@gmail.com>
2026-09-18 14:47:34 +0200

    vault backup: 2026-09-18 14:47:34

--- First Semester/Challenge 1/Creating a game - RL/Godot/the-witness/Scripts/player_character.gd
+++ First Semester/Challenge 1/Creating a game - RL/Godot/the-witness/Scripts/player_character.gd
@@ -1,18 +1,31 @@
 extends CharacterBody2D
  
-## This is the speed, and with @export_range we are creating a var so we can
+## This is the speed,acceleration and friction and with @export_range we are creating a var so we can
 ## change the speed withouth having to touch the code
-@export_range(0, 1000) var speed := 60
+@export_range(0, 1000) var base_speed := 60
 @export var accel: float = 1200.0
 @export var friction: float = 1400.0
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 var lastinput = "Idle_Up"
+var is_crouching = false #Saves if the character is crouch
+
 func _physics_process(delta: float) -> void:
+	# 1. Detect movement entries
 	var input := Input.get_vector("left", "right", "up", "down")
+	
+	# Modo Mantener: is_crouching sera True solo mientras mantenga pulsado el Control
+	is_crouching = Input.is_action_pressed("crouch")
+	var current_speed = base_speed
+	if is_crouching:
+		current_speed = base_speed * 0.5 #It walks slower if the player is crouch
+	elif Input.is_action_pressed("shift"):
+		current_speed = base_speed * 1.6 #Calculates de base speed and multiplies per 1.6 to Run
+	
 	if input != Vector2.ZERO:
-		velocity = velocity.move_toward(input * speed, accel * delta)
+		velocity = velocity.move_toward(input * current_speed, accel * delta)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
+	move_and_slide()
 	
 	if Input.is_action_pressed("down") && Input.is_action_pressed("right"):
 		sprite.play("Walking_Down_R")
@@ -30,17 +43,27 @@ func _physics_process(delta: float) -> void:
 		sprite.play("Walking_Up")
 		lastinput = "Idle_Up"
 	elif Input.is_action_pressed("down"):
-		sprite.play("Walking_Down")
-		lastinput = "Idle_Down"
+		if Input.is_action_pressed("shift"):
+			sprite.play("Running_Down")
+		else:
+			sprite.play("Walking_Down")
+			lastinput = "Idle_Down"
+		
 	elif Input.is_action_pressed("right"):
 		sprite.play("Walking_Right")
 		lastinput = "Idle_Right"
 	elif Input.is_action_pressed("left"):
 		sprite.play("Walking_Left")
 		lastinput = "Idle_Left"
+#	elif Input.is_action_pressed("shift") && Input.is_action_pressed("down"):
+	#	sprite.play("Running_Down")
+	#	lastinput = "Idle_Down"
+	elif Input.is_action_pressed("shift") && Input.is_action_pressed("up"):
+		sprite.play("Running_Up")
+		lastinput = "Idle_Up"
+	
 
 	else:
 		sprite.play(lastinput)
-	move_and_slide()
 	
 	
