c17831f4d5c89347b702291017385009e59febaa
Eslavic <deslavajose@gmail.com>
2026-09-20 23:15:33 +0200

    vault backup: 2026-09-20 23:15:33

--- First Semester/Challenge 1/Creating a game - RL/Godot/the-witness/Scripts/player_character.gd
+++ First Semester/Challenge 1/Creating a game - RL/Godot/the-witness/Scripts/player_character.gd
@@ -6,9 +6,11 @@ extends CharacterBody2D
 @export var accel: float = 1200.0
 @export var friction: float = 1400.0
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+# We create a list organized by states:
+enum State { IDLE, WALKING, RUNNING, CROUCHING}
+var curren_state = State.IDLE
 var lastinput = "Idle_Up"
 var is_crouching = false #Saves if the character is crouch
-
 func _physics_process(delta: float) -> void:
 	# 1. Detect movement entries
 	var input := Input.get_vector("left", "right", "up", "down")
@@ -16,9 +18,12 @@ func _physics_process(delta: float) -> void:
 	# Modo Mantener: is_crouching sera True solo mientras mantenga pulsado el Control
 	is_crouching = Input.is_action_pressed("crouch")
 	var current_speed = base_speed
+	var _state = "walking"
 	if is_crouching:
+		_state = "Crouching"
 		current_speed = base_speed * 0.5 #It walks slower if the player is crouch
 	elif Input.is_action_pressed("shift"):
+		_state = "Running"
 		current_speed = base_speed * 5 #Calculates de base speed and multiplies per 1.6 to Run
 	
 	if input != Vector2.ZERO:
@@ -26,5 +31,4 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
 	move_and_slide()
-		
 	
