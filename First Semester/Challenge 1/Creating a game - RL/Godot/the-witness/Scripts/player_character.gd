eba8a63dac33eed8ae74899dadf1f4e9d71c7624
Eslavic <deslavajose@gmail.com>
2026-09-20 23:54:16 +0200

    vault backup: 2026-09-20 23:54:16

--- First Semester/Challenge 1/Creating a game - RL/Godot/the-witness/Scripts/player_character.gd
+++ First Semester/Challenge 1/Creating a game - RL/Godot/the-witness/Scripts/player_character.gd
@@ -6,68 +6,83 @@ extends CharacterBody2D
 @export var accel: float = 1200.0
 @export var friction: float = 1400.0
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
-# We create a list organized by states:
-enum State { IDLE, WALKING, RUNNING, CROUCHING}
-# We save our current_state in IDLE
-var current_state = State.IDLE
 var lastinput = "Idle_Up"
-#var is_crouching = false #Saves if the character is crouch
+var is_crouching = false #Saves if the character is crouch
+
 func _physics_process(delta: float) -> void:
 	# 1. Detect movement entries
 	var input := Input.get_vector("left", "right", "up", "down")
 	
 	# Modo Mantener: is_crouching sera True solo mientras mantenga pulsado el Control
-	#is_crouching = Input.is_action_pressed("crouch")
+	is_crouching = Input.is_action_pressed("crouch")
 	var current_speed = base_speed
-	if input == Vector2.ZERO:
-		current_state = State.IDLE
-	elif Input.is_action_pressed("crouch"):
-		current_state = State.CROUCHING
+	if is_crouching:
+		current_speed = base_speed * 0.5 #It walks slower if the player is crouch
 	elif Input.is_action_pressed("shift"):
-		current_state = State.RUNNING
+		current_speed = base_speed * 5 #Calculates de base speed and multiplies per 1.6 to Run
+	
+	if input != Vector2.ZERO:
+		velocity = velocity.move_toward(input * current_speed, accel * delta)
 	else:
-		current_state = State.WALKING
-		
-	match current_state:
-		State.IDLE:
-			velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
-		State.CROUCHING:
-			current_speed = base_speed * 0.5 #It walks slower if the player is crouch
-			velocity = velocity.move_toward(input * current_speed, accel * delta)
-		State.WALKING:
-			current_speed = base_speed
-			velocity = velocity.move_toward(input * current_speed, accel * delta)
-		State.RUNNING:
-			#Calculates de base speed and multiplies per 5 to Run
-			current_speed = base_speed * 5
-			velocity = velocity.move_toward(input * current_speed, accel * delta)
+		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
 	move_and_slide()
-	# Convertimos el vector a enteros (Vector2i) con .sign() para que sea exacto (-1, 0 o 1)
-	var dir_key = Vector2i(input.sign())
+	
+	if Input.is_action_pressed("down") && Input.is_action_pressed("right"):
+		if Input.is_action_pressed("shift"):
+			sprite.play("Running_Down_R")
+		else:			
+			sprite.play("Walking_Down_R")
+			lastinput = "Idle_Down_R"
+	elif Input.is_action_pressed("up") && Input.is_action_pressed("right"):
+		if Input.is_action_pressed("shift"):
+			sprite.play("Running_Up_R")
+		else:
+			sprite.play("Walking_Up_R")
+			lastinput = "Idle_Up_R"
+	elif Input.is_action_pressed("down") && Input.is_action_pressed("left"):
+		if Input.is_action_pressed("shift"):
+			sprite.play("Running_Down_L")
+		else:
+			sprite.play("Walking_Down_L")
+			lastinput = "Idle_Down_L"
+	elif Input.is_action_pressed("up") && Input.is_action_pressed("left"):
+		if Input.is_action_pressed("shift"):
+			sprite.play("Running_Up_L")
+		else:
+			sprite.play("Walking_Up_L")
+			lastinput = "Idle_Up_L"
+	elif Input.is_action_pressed("up"):
+		if Input.is_action_pressed("shift"):
+			sprite.play("Running_Up")
+		else:
+			sprite.play("Walking_Up")
+			lastinput = "Idle_Up"
+	elif Input.is_action_pressed("down"):
+		if Input.is_action_pressed("shift"):
+			sprite.play("Running_Down")
+		else:
+			sprite.play("Walking_Down")
+			lastinput = "Idle_Down"
 		
-	match current_state:
-		State.IDLE:
-			sprite.play(lastinput)
-		
-		State.WALKING, State.RUNNING:
-			var prefijo = "Walking_" if current_state == State.WALKING else "Running_"
-			var anim_dir = ""
+	elif Input.is_action_pressed("right"):
+		if Input.is_action_pressed("shift"):
+			sprite.play("Running_Right")
+		else:
+			sprite.play("Walking_Right")
+			lastinput = "Idle_Right"
+	elif Input.is_action_pressed("left"):
+		if Input.is_action_pressed("shift"):
+			sprite.play("Running_Left")
+		else:
+			sprite.play("Walking_Left")
+			lastinput = "Idle_Left"
+#	elif Input.is_action_pressed("shift") && Input.is_action_pressed("down"):
+	#	sprite.play("Running_Down")
+	#	lastinput = "Idle_Down"
+	elif Input.is_action_pressed("shift") && Input.is_action_pressed("up"):
+		sprite.play("Running_Up")
+		lastinput = "Idle_Up"
+	
 
-			
-	
-	
-	
-	
-	
-	#if is_crouching:
-		#_state = "Crouching"
-		
-	#elif Input.is_action_pressed("shift"):
-		#_state = "Running"
-	#
-	#if input != Vector2.ZERO:
-		#velocity = velocity.move_toward(input * current_speed, accel * delta)
-	#else:
-		##velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
-	#move_and_slide()
-	#
+	else:
+		sprite.play(lastinput)
